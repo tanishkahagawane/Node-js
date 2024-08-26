c0f711f086cae23cf97af551997677001e60cefd
tanishkahagawane <tanishkahagawane@gmail.com>
2024-08-26 15:17:28 +0530

    Simple API

---
+++ 7_simple_API/data.js
@@ -0,0 +1,6 @@
+const data =[
+    {name:'Tanishka H',email:'tan@test.com', contact:'7777'},
+    {name:'Anishka ',email:'A@test.com', contact:'72277'},
+    {name:'nish',email:'nish@test.com', contact:'7447'}
+];
+module.exports=data;
