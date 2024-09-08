b8c860fac30001a73ded7689fa18248ad360659d
tanishkahagawane <tanishkahagawane@gmail.com>
2024-09-08 11:31:03 +0530

    Node JS architecture

--- 12_How_NodeJS_works/index.js
+++ 12_How_NodeJS_works/index.js
@@ -21,3 +21,11 @@ console.log("completed");
              - then resgisters in CallbackQueue 
              - once CallStack is clear then its excuted in callstack
 */
+
+/*
+At this point let's assume that setTimeout, the one that has a zero second delay, finishes.
+When it finishes, it's not going to get executed right away; it's going to take that callback and move it down into the Callback Queue.
+The Callback Queue is all the callback functions that are ready to get fired. In the previous screenshot, we move the function from Node API into the Callback Queue. Now the Callback Queue is where our callback functions will wait; they need to wait for the Call Stack to be empty.
+When the Call Stack is empty we can run the first function. There's another function ...
+
+*/
